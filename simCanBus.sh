8f220cf08097ed14d2aa4a6fd5dbaedd0ca555b1
Steffen Köhler <steffen@koehlers.de>
2023-07-23 08:46:36 +0200

    first msgs on can 1 & 2

---
+++ simCanBus.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+# do the inital settings for a virtual car connected by a real or virtual CAN-Bus, realized by socketCAN devices
+
+BAUDRATE=250000
+BRMASTER=250000
+CAN0=can0
+CAN1=can1
+
+echo "Use Baudrate of $BAUDRATE"
+sudo modprobe can
+sudo modprobe can_raw
+sudo modprobe can_bcm
+sudo modprobe vcan
+#sudo ip link add dev vcan0 type vcan
+#sudo ip link set up vcan0
+
+PS3='Please take your choice: '
+options=("bitrate 500k" "bitrate 250k" "bitrate 125k" "(re)start bus" "show can state" "Quit")
+select opt in "${options[@]}"
+	do
+		case $opt in
+
+			"bitrate 125k")
+				echo "you chose bitrate 125k"
+				BAUDRATE=125000
+				echo "bitrate set to $BAUDRATE"
+
+			;;
+			"bitrate 250k")
+				echo "you chose bitrate 250k"
+				BAUDRATE=250000
+				echo "bitrate set to $BAUDRATE"
+
+			;;
+			"bitrate 500k")
+				echo "you chose bitrate 500k"
+				BAUDRATE=500000
+				echo "bitrate set to $BAUDRATE"
+
+			;;
+			"(re)start bus")
+				echo "you chose (re)start bus"
+        if [ -n "$can0_pid" ] ; then
+					kill $can0_pid
+					unset can0_pid
+				fi
+        if [ -n "$can1_pid" ] ; then
+					kill $can1_pid
+					unset can1_pid
+				fi
+        if ip link show $CAN0 ; then
+          sudo ifconfig $CAN0 down
+        fi
+        if ip link show $CAN1 ; then
+          sudo ifconfig $CAN1 down
+        fi
+        sudo ip link set $CAN0 type can bitrate $BRMASTER triple-sampling on
+        sudo ifconfig $CAN0 up
+        sudo ip link set $CAN1 type can bitrate $BAUDRATE triple-sampling on
+        sudo ifconfig $CAN1 up
+        candump $CAN0 &
+				can0_pid=$!
+        cangen  $CAN1 &
+				can1_pid=$!
+
+			;;
+
+			"show can state")
+				echo "you chose show can state"
+				ip -details link show $CAN0
+				ip -details link show $CAN1
+			;;
+			"Quit")
+        if [ -n "$can0_pid" ] ; then
+					kill $can0_pid
+					unset can0_pid
+				fi
+        if [ -n "$can1_pid" ] ; then
+					kill $can1_pid
+					unset can1_pid
+				fi
+				break
+			;;
+			*) echo invalid option;;
+		esac
+	done
